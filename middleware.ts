39174be839f7cbbdbab06e777280ef17d1cea534
Santhosh VS <santhosh02vs@gmail.com>
2026-09-16 20:11:54 +0530

    fix(auth): add /about to public routes in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,11 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/"]);
+const isPublicRoute = createRouteMatcher([
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/",
+  "/about(.*)",
+]);
 
 export default clerkMiddleware(async (auth, req) => {
   // Add your middleware checks
